8c315b3a117dd66d335165a9fd7d9701e2b5a2db
Felipe <felipe.lourenco.ca@gmail.com>
2022-09-13 14:02:43 -0300

    Recursao e introdução a programação orientada a objetos

---
+++ 4_POO-Recursao/main.js
@@ -0,0 +1,20 @@
+// Estrutura    
+class Heroi{
+    //nome   string nome
+    //forca  int forca
+
+    constructor(nome,forca){
+        this.nome = nome;
+        this.forca = forca;
+    }
+    
+}
+// programa
+
+//var nome = prompt("Digite o nome do heroi");
+//var forca = parseFloat(prompt("Digite a forca do heroi"));
+/*
+console.log("O nome eh: " + nome + "a forca eh: " + forca);
+*/
+
+
